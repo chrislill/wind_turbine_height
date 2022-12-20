31134fefba903ed46ea8d565ab759901101f4d74
Chris Lill <chris_lill_uk@hotmail.com>
2022-12-20 16:49:28 +0000

    Crop all orthophotos use the right zone

--- wind_turbine_height/crop_orthophotos.py
+++ wind_turbine_height/crop_orthophotos.py
@@ -4,40 +4,42 @@ from osgeo import gdal
 
 
 def main():
-    dotenv.load_dotenv('.env')
-    dotenv.load_dotenv('.env.secret')
+    dotenv.load_dotenv(".env")
+    dotenv.load_dotenv(".env.secret")
 
     # Clip each image to 2km * 2km, centered on the wind site
     clip_width = 2000
 
     # Load site photo metadata
-    site_metadata = pd.read_csv('data/site_photo_metadata.csv')
+    site_metadata = pd.read_csv("data/site_photo_metadata.csv")
 
     for _, site in site_metadata.iterrows():
-        print(f'Cropping {site.site}')
-        orthophoto = gdal.Open(f'data/orthophotos/{site.orthophoto_name}.ecw')
-        source_info = gdal.Info(orthophoto, format='json')
-        resolution = source_info['geoTransform'][1]
+        print(f"Cropping {site.site}")
+        orthophoto = gdal.Open(f"data/orthophotos/{site.orthophoto_name}.ecw")
+        source_info = gdal.Info(orthophoto, format="json")
+        resolution = source_info["geoTransform"][1]
         left_offset = int(
-            (site.site_x - source_info['geoTransform'][0] - (clip_width / 2)) / resolution
+            (site.site_x - source_info["geoTransform"][0] - (clip_width / 2))
+            / resolution
         )
         top_offset = int(
-            (source_info['geoTransform'][3] - site.site_y - (clip_width / 2)) / resolution
+            (source_info["geoTransform"][3] - site.site_y - (clip_width / 2))
+            / resolution
         )
         gdal.Translate(
-            f'data/site_images/{site.site}.png',
+            f"data/site_images/{site.site}.png",
             orthophoto,
-            format='PNG',
+            format="PNG",
             srcWin=[
                 left_offset,
                 top_offset,
                 int(clip_width / resolution),
-                int(clip_width / resolution)
-            ]
+                int(clip_width / resolution),
+            ],
         )
 
-    print('Done')
+    print("Done")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- wind_turbine_height/load_photo_metadata.py
+++ wind_turbine_height/load_photo_metadata.py
@@ -11,25 +11,30 @@ import pyodbc
 
 
 def main():
-    dotenv.load_dotenv('.env')
-    dotenv.load_dotenv('.env.secret')
+    dotenv.load_dotenv(".env")
+    dotenv.load_dotenv(".env.secret")
 
     # Load Spanish wind farm metadata, provided by Aurora Energy Research
     # Some wind farms have multiple turbine configurations
-    site_metadata = pd.read_csv(os.getenv('site_metadata_path'))
+    site_metadata = pd.read_csv(os.getenv("site_metadata_path"))
     sites = (
-        site_metadata
-        .query('selected == True')
-        .groupby('site')
-        .agg({
-            'latitude': 'mean',
-            'longitude': 'mean',
-            'num_turbines': lambda x: tuple(x),
-            'hub_height': lambda x: tuple(x)
-        })
+        site_metadata.query("selected == True")
+        .groupby("site")
+        .agg(
+            {
+                "latitude": "mean",
+                "longitude": "mean",
+                "num_turbines": lambda x: tuple(x),
+                "hub_height": lambda x: tuple(x),
+            }
+        )
         .sort_index()
         .reset_index()
-        .assign(geometry=lambda x: gpd.points_from_xy(x.longitude, x.latitude, crs="EPSG:4326"))
+        .assign(
+            geometry=lambda x: gpd.points_from_xy(
+                x.longitude, x.latitude, crs="EPSG:4326"
+            )
+        )
         .set_geometry("geometry")
         .to_crs("EPSG:25830")
         .assign(
@@ -40,37 +45,71 @@ def main():
 
     # Load shape files giving the location of each orthophoto tile
     shutil.unpack_archive(
-        'data/photo_metadata/fechas_ortofotos_PNOA_MA.zip',
-        'data/photo_metadata/orthophoto_tiles'
+        "data/photo_metadata/fechas_ortofotos_PNOA_MA.zip",
+        "data/photo_metadata/orthophoto_tiles",
     )
     orthophoto_tiles = gpd.read_file(
-        'data/photo_metadata/orthophoto_tiles/20220923_Estado_Mosaicos_109_MA'  # noqa
-        '/20220923_Estado_Mosaicos_MA_MR.shp'  # noqa
+        "data/photo_metadata/orthophoto_tiles/20220923_Estado_Mosaicos_109_MA"  # noqa
+        "/20220923_Estado_Mosaicos_MA_MR.shp"  # noqa
     ).assign(tile_centroid=lambda x: x.geometry.centroid)
 
-    # Join tiles to sites. If there are duplicates keep the one where the site
-    # is closest to the centroid of the tile
+    # Join orthophoto tiles to sites. If there are duplicates keep the one where
+    # the site is closest to the centroid of the tile
     site_tiles = (
         gpd.sjoin(sites, orthophoto_tiles, how="left", predicate="within")
         .assign(
             distance_to_centroid=lambda x: x.distance(x.tile_centroid),
             orthophoto_name=lambda x: (
-                    "PNOA_MA_OF_ETRS89_HU" + x.HUSO.astype(str) + "_H50_" + x.HMTN50.astype(str)
-            )
+                "PNOA_MA_OF_ETRS89_HU"
+                + x.HUSO.astype(str)
+                + "_H50_"
+                + x.HMTN50.astype(str)
+            ),
         )
         .sort_values(["site", "distance_to_centroid"])
         .drop_duplicates("site")
         .drop(columns="index_right")
     )
+
+    # The orthophotos actually use three different projections for UTM zones 29, 30 and 31
+    # Site coordinates are already in 25830. Fix it for 29 and 31
+    # https://epsg.io/25829, https://epsg.io/25830, https://epsg.io/25831
+    for zone in [29, 31]:
+        zone_tiles = (
+            site_tiles.query(f"HUSO == '{zone}'")
+            .to_crs(f"EPSG:258{zone}")
+            .assign(
+                site_x=lambda x: x.geometry.x.round(2),
+                site_y=lambda x: x.geometry.y.round(2),
+            )
+        )
+        site_tiles.loc[site_tiles.HUSO.eq(str(zone)), ["site_x", "site_y"]] = zone_tiles[
+            ["site_x", "site_y"]
+        ]
+
+    # Join with photo metadata to find timestamp of the nearest photo
     photo_metadata = load_photo_metadata()
     site_photos = (
-        gpd.sjoin_nearest(site_tiles, photo_metadata, how="left", distance_col="photo_distance")
+        gpd.sjoin_nearest(
+            site_tiles, photo_metadata, how="left", distance_col="photo_distance"
+        )
         .query("photo_distance < 3100")
         .reset_index(drop=True)
-        .loc[:, [
-            "site", "latitude", "longitude", "num_turbines", "hub_height", "photo_file",
-            "photo_timestamp", "orthophoto_name", "site_x", "site_y"
-        ]]
+        .loc[
+            :,
+            [
+                "site",
+                "latitude",
+                "longitude",
+                "num_turbines",
+                "hub_height",
+                "photo_file",
+                "photo_timestamp",
+                "orthophoto_name",
+                "site_x",
+                "site_y",
+            ],
+        ]
     )
     site_photos.to_csv("data/site_photo_metadata.csv", index=False)
     print("Done")
@@ -78,7 +117,7 @@ def main():
 
 def load_photo_metadata():
     """Load aerial photo metadata from zipped access database files"""
-    metadata_path = pathlib.Path('data/photo_metadata')
+    metadata_path = pathlib.Path("data/photo_metadata")
     database_files = list(metadata_path.glob("**/PNOA*.mdb"))
     if len(database_files) > 0:
         # TODO: Implement logging
@@ -96,7 +135,8 @@ def load_photo_metadata():
                     for zipped_file in zipped_files:
                         if pnoa_regex.match(zipped_file.filename):
                             with open(
-                                metadata_path / zipped_file.filename.split("/")[-1], "wb"
+                                metadata_path / zipped_file.filename.split("/")[-1],
+                                "wb",
                             ) as f:
                                 f.write(zp.read(zipped_file.filename))
         database_files = list(metadata_path.glob("**/PNOA*.mdb"))
@@ -106,7 +146,7 @@ def load_photo_metadata():
     photo_list = []
     for database_file in database_files:
         connection_string = (
-            r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};" 
+            r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
             rf"DBQ={str(database_file.resolve())};"
         )
         try:
@@ -117,16 +157,22 @@ def load_photo_metadata():
                         "SELECT FOTOGRAMA_TIFF, FECHA, HORA, LAT_ETRS89, LONG_ETRS89 "  # noqa
                         "FROM VueloEjecutado"  # noqa
                     ).fetchall(),
-                    columns=["photo_file", "date", "time", "photo_latitude", "photo_longitude"]
+                    columns=[
+                        "photo_file",
+                        "date",
+                        "time",
+                        "photo_latitude",
+                        "photo_longitude",
+                    ],
                 )
 
                 # There is a mixture of datetime objects and strings. Format them all as strings
                 # Then combine
-                if pd.api.types.infer_dtype(results.date) == 'string':
+                if pd.api.types.infer_dtype(results.date) == "string":
                     results.date = results.date.str.slice(0, 11)
                 else:
                     results.date = results.date.dt.strftime(date_format="%d/%m/%Y")
-                if pd.api.types.infer_dtype(results.time) == 'string':
+                if pd.api.types.infer_dtype(results.time) == "string":
                     results.time = (
                         results.time.str.strip()
                         .str.replace("60", "00")
@@ -134,13 +180,11 @@ def load_photo_metadata():
                     )
                 else:
                     results.time = results.time.dt.strftime(date_format="%X")
-                results = (
-                    results
-                    .assign(photo_timestamp=lambda x: pd.to_datetime(
+                results = results.assign(
+                    photo_timestamp=lambda x: pd.to_datetime(
                         x.date + " " + x.time, dayfirst=True, utc=True
-                    ))
-                    .drop(columns=["date", "time"])
-                )
+                    )
+                ).drop(columns=["date", "time"])
                 photo_list.append(results)
 
         # TODO: Can we fix these errors. 16/91 labels are being dropped
@@ -163,5 +207,5 @@ def load_photo_metadata():
     return photos
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
