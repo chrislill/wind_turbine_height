e64c2b8ec56d26cf5b96bf7b888edf976979028a
Chris Lill <chris_lill_uk@hotmail.com>
2022-12-20 15:45:21 +0000

    Loop through orthophotos

--- wind_turbine_height/crop_orthophotos.py
+++ wind_turbine_height/crop_orthophotos.py
@@ -1,8 +1,5 @@
-import os
-
 import dotenv
 import pandas as pd
-import rasterio as rio
 from osgeo import gdal
 
 
@@ -10,23 +7,34 @@ def main():
     dotenv.load_dotenv('.env')
     dotenv.load_dotenv('.env.secret')
 
+    # Clip each image to 2km * 2km, centered on the wind site
+    clip_width = 2000
+
     # Load site photo metadata
     site_metadata = pd.read_csv('data/site_photo_metadata.csv')
 
-    site = site_metadata[site_metadata.site.eq('brulles')].iloc[0]
-    # with gdal.Open(f'data/orthophotos/{site.orthophoto_name}.ecw') as orthophoto:
-    orthophoto = gdal.Open(f'data/orthophotos/{site.orthophoto_name}.ecw')
-    source_info = gdal.Info(orthophoto, format='json')
-    if source_info['geoTransform'][1] != 0.25:
-        raise ValueError("Orthophoto resolution is not 0.25m - Implement different resolutions")
-    left_offset = int((site.site_x - source_info['geoTransform'][0] - 1000) / 0.25)
-    top_offset = int((source_info['geoTransform'][3] - site.site_y - 1000) / 0.25)
-    gdal.Translate(
-        f'data/site_images/{site.site}.png',
-        orthophoto,
-        format='PNG',
-        srcWin=[left_offset, top_offset, 8000, 8000]
-    )
+    for _, site in site_metadata.iterrows():
+        print(f'Cropping {site.site}')
+        orthophoto = gdal.Open(f'data/orthophotos/{site.orthophoto_name}.ecw')
+        source_info = gdal.Info(orthophoto, format='json')
+        resolution = source_info['geoTransform'][1]
+        left_offset = int(
+            (site.site_x - source_info['geoTransform'][0] - (clip_width / 2)) / resolution
+        )
+        top_offset = int(
+            (source_info['geoTransform'][3] - site.site_y - (clip_width / 2)) / resolution
+        )
+        gdal.Translate(
+            f'data/site_images/{site.site}.png',
+            orthophoto,
+            format='PNG',
+            srcWin=[
+                left_offset,
+                top_offset,
+                int(clip_width / resolution),
+                int(clip_width / resolution)
+            ]
+        )
 
     print('Done')
 
