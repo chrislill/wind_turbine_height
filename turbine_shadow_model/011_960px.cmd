24a93b99525ff5a33039c0b3cb49106ad1dc77f9
Chris Lill <chris_lill_uk@hotmail.com>
2023-01-02 22:27:08 +0000

    Try 2 x 2 x 960px

---
+++ turbine_shadow_model/011_960px.cmd
@@ -0,0 +1,13 @@
+SET experiment=011_960px
+SET run=
+SET data=v011
+SET yolo_path=%USERPROFILE%/Code/yolov7
+SET weights_path=%yolo_path%/weights/yolov7_training.pt
+:: SET weights_path=.\runs\train\%experiment%%run%\weights\last.pt
+SET hyp_path=turbine_shadow_model/hyp.scratch.custom.yaml
+:: SET hyp_path=turbine_shadow_model/hyp.no_shear.yaml
+
+CALL CD %USERPROFILE%\Code\wind_turbine_height
+CALL yolov7_venv\scripts\activate
+CALL wandb disabled
+CALL python %yolo_path%/train.py --epochs 500 --img-size 960 --batch 2 --workers 4 --cfg %yolo_path%/cfg/training/yolov7.yaml --hyp %hyp_path% --data data/turbine_shadow_data/%data%/data.yaml --weights %weights_path% --device 0 --name %experiment%
