dc083ba401a1d7bdb500409432ff13f4bb62f083
Chris Lill <chris_lill_uk@hotmail.com>
2023-01-14 17:03:21 +0000

    Integrate wandb

--- hub_shadow_model/001_initial_model.cmd
+++ hub_shadow_model/001_initial_model.cmd
@@ -1,13 +1,13 @@
 SET experiment=001_initial_model
-SET run=1
+SET run=6
 SET data=v001
 SET yolo_path=%USERPROFILE%/Code/yolov7
-SET weights_path=%yolo_path%/weights/yolov7_training.pt
-::SET weights_path=.\runs\train\%experiment%%run%\weights\last.pt
+:: SET weights_path=%yolo_path%/weights/yolov7_training.pt
+SET weights_path=.\runs\train\%experiment%%run%\weights\last.pt
 SET hyp_path=hyp.scratch.custom.yaml
 :: SET hyp_path=hyp.no_shear.yaml
 
 CALL CD %USERPROFILE%\Code\wind_turbine_height\hub_shadow_model
-CALL ../yolov7_venv\scripts\activate
-CALL wandb disabled
-CALL python %yolo_path%/train.py --epochs 100 --img-size 640 --batch 4 --workers 4 --cfg %yolo_path%/cfg/training/yolov7.yaml --hyp %hyp_path% --data ../data/hub_shadow_data/%data%/data.yaml --weights %weights_path% --device 0 --name %experiment%
+CALL ..\yolov7_venv\scripts\activate
+:: CALL wandb disabled
+CALL python %yolo_path%/train.py --epochs 500 --img-size 640 --batch 4 --workers 4 --cfg %yolo_path%/cfg/training/yolov7.yaml --hyp %hyp_path% --data ../data/hub_shadow_data/%data%/data.yaml --weights %weights_path% --device 0 --name %experiment% --entity data_science_msc
