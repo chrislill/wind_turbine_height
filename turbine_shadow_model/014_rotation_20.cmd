94ae00b289e5a8113603d98ff137dda86f7830d2
Chris Lill <chris_lill_uk@hotmail.com>
2023-02-08 17:59:57 +0000

    Rotation and shear experiments

---
+++ turbine_shadow_model/014_rotation_20.cmd
@@ -0,0 +1,14 @@
+SET experiment=014_shear_20
+SET baseline=012_baseline
+SET run=
+SET data=v013
+SET yolo_path=%USERPROFILE%/Code/yolov7
+:: SET weights_path=%yolo_path%/weights/yolov7_training.pt
+SET weights_path=.\runs\train\%baseline%%run%\weights\best.pt
+SET hyp_path=hyperparameters\hyp.shear_20.yaml
+:: SET hyp_path=hyp.no_shear.yaml
+
+CALL CD %USERPROFILE%\Code\wind_turbine_height\turbine_shadow_model
+CALL ..\yolov7_venv\scripts\activate
+CALL wandb disabled
+CALL python %yolo_path%/train.py --epochs 200 --img-size 640 --batch 4 --workers 4 --cfg %yolo_path%/cfg/training/yolov7.yaml --hyp %hyp_path% --data ../data/turbine_shadow_data/%data%/data.yaml --weights %weights_path% --device 0 --name %experiment%
--- turbine_shadow_model/start_tensorboard.cmd
+++ turbine_shadow_model/start_tensorboard.cmd
@@ -1,3 +1,3 @@
 CALL CD %USERPROFILE%\Code\wind_turbine_height\turbine_shadow_model
 CALL ..\yolov7_venv\scripts\activate
-CALL tensorboard --logdir turbine_shadow
+CALL tensorboard --logdir runs\train
