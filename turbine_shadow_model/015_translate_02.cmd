e624a015c89d1dea9755c4c68e92632770fd78c7
Chris Lill <chris_lill_uk@hotmail.com>
2023-02-13 08:24:55 +0000

    Additional experiments

---
+++ turbine_shadow_model/015_translate_02.cmd
@@ -0,0 +1,16 @@
+SET experiment=015_translate_02
+SET baseline=009_4_tiles
+SET run=4
+SET data=v013
+SET yolo_path=%USERPROFILE%/Code/yolov7
+:: SET weights_path=%yolo_path%/weights/yolov7_training.pt
+SET weights_path=.\runs\train\%baseline%%run%\weights\best.pt
+:: SET weights_path=.\runs\train\%experiment%\weights\last.pt
+SET hyp_path=hyperparameters\hyp.translate_02.yaml
+:: SET hyp_path=hyp.no_shear.yaml
+
+CALL CD %USERPROFILE%\Code\wind_turbine_height\turbine_shadow_model
+CALL ..\yolov7_venv\scripts\activate
+CALL wandb disabled
+CALL python %yolo_path%/train.py --epochs 200 --img-size 640 --batch 4 --workers 4 --cfg %yolo_path%/cfg/training/yolov7.yaml --hyp %hyp_path% --data ../data/turbine_shadow_data/%data%/data.yaml --weights %weights_path% --device 0 --name %experiment%
+:: --resume
