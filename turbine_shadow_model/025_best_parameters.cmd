5357019109abc18d31966772cd1991145e99785b
Chris Lill <chris_lill_uk@hotmail.com>
2023-02-22 16:50:46 +0000

    Select best turbine shadow model

---
+++ turbine_shadow_model/025_best_parameters.cmd
@@ -0,0 +1,15 @@
+SET experiment=025_best_parameters
+SET baseline=009_4_tiles
+SET run=3
+SET data=v013_additional
+SET yolo_path=%USERPROFILE%/Code/yolov7
+SET hyp_path=hyperparameters\hyp.best_parameters.yaml
+
+SET weights_path=.\runs\train\%baseline%%run%\weights\best.pt
+:: SET weights_path=.\runs\train\%experiment%\weights\last.pt
+
+CALL CD %USERPROFILE%\Code\wind_turbine_height\turbine_shadow_model
+CALL ..\yolov7_venv\scripts\activate
+CALL wandb disabled
+CALL python %yolo_path%/train.py --epochs 200 --img-size 640 --batch 4 --workers 4 --cfg %yolo_path%/cfg/training/yolov7.yaml --hyp %hyp_path% --data ../data/turbine_shadow_data/%data%/data.yaml --weights %weights_path% --device 0 --name %experiment%
+:: --resume
--- turbine_shadow_model/detect_turbine_shadows.cmd
+++ turbine_shadow_model/detect_turbine_shadows.cmd
@@ -1,12 +1,12 @@
-SET experiment=012_baseline
+SET experiment=026_additional_labels_mixup
 SET run=
 SET yolo_path=%USERPROFILE%/Code/yolov7
 SET weights_path=.\runs\train\%experiment%%run%\weights\best.pt
 SET image_file=
 ::SET weights_path=%yolo_path%/weights/yolov7-e6e.pt
 
-SET image_path=..\data\turbine_shadow_data\active_learning\test\images
-
+SET image_path=..\data\turbine_shadow_data\v013\test\images
+::SET image_path=..\data\turbine_shadow_data\active_learning\test\images
 
 CALL CD %USERPROFILE%\Code\wind_turbine_height\turbine_shadow_model
 CALL ..\yolov7_venv\scripts\activate
