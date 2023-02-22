65ee19e65a1e7d9f681b289625af3babff1e8c0f
Chris Lill <chris_lill_uk@hotmail.com>
2023-02-22 18:23:23 +0000

    Experiment without default transforms

--- hub_shadow_model/detect_hub_shadows.cmd
+++ hub_shadow_model/detect_hub_shadows.cmd
@@ -1,15 +1,14 @@
-SET experiment=001_initial_model
-SET run=1
+SET experiment=002_baseline
+SET run=
 SET data=v001
 SET yolo_path=%USERPROFILE%/Code/yolov7
 SET weights_path=.\runs\train\%experiment%%run%\weights\best.pt
-SET image_file=almendarache_png.rf.66c76fdb89707ab8223492f51c83e1af.jpg
 ::SET weights_path=%yolo_path%/weights/yolov7-e6e.pt
 ::SET image_file=man_cafe.jpg
-SET image_path=..\data\hub_shadow_data\%data%\test\images\%image_file%
+SET image_path=..\data\hub_shadow_data\%data%\test\images
 
 
 CALL CD %USERPROFILE%\Code\wind_turbine_height\hub_height_model
 CALL ..\yolov7_venv\scripts\activate
 CALL wandb disabled
-CALL python %yolo_path%/detect.py --conf 0.60 --weights %weights_path% --source %image_path%
+CALL python %yolo_path%/detect.py --conf 0.50 --weights %weights_path% --source %image_path%
