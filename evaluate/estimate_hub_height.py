034496e1cb7ae8449756ff89a4b26e95416d0d4d
Chris Lill <chris_lill_uk@hotmail.com>
2023-02-26 16:28:06 +0000

    Summarise test results

--- evaluate/estimate_hub_height.py
+++ evaluate/estimate_hub_height.py
@@ -8,17 +8,16 @@ import dotenv
 import numpy as np
 import pandas as pd
 from skyfield import api as skyfield_api
-from osgeo import gdal
+from osgeo import gdal  # noqa
 
 
-def main():
+def main(run_name):
     dotenv.load_dotenv(".env")
     dotenv.load_dotenv(".env.secret")
 
     sites = pd.read_csv("data/site_photo_metadata.csv")
     orthophotos = pd.read_csv("data/orthophoto_metadata.csv")
-    test_folder = os.getenv("labelled_hub_shadow_folder")
-    label_paths = Path(f"hub_shadow_model/runs/test/{test_folder}/labels").glob("*")
+    label_paths = Path(f"hub_shadow_model/runs/detect/{run_name}/labels").glob("*")
 
     # Set up skyfield to calculate relative positions of the earth and sun
     ephemeris = skyfield_api.load("de421.bsp")
@@ -41,23 +40,25 @@ def main():
         else:
             actual_hub_height = np.nan
 
-        # The predicted labels are listed in order of confidence
         labels = pd.read_csv(
             label_path,
             sep=" ",
-            names=["label", "center_x", "center_y", "width", "height"],
+            names=["label", "center_x", "center_y", "width", "height", "confidence"],
         )
-        if labels.label.eq(0).sum() >= 1 and labels.label.eq(1).sum() >= 1:
+        turbine_metadata = {
+            "site": site,
+            "actual_hub_height": actual_hub_height,
+            "num_bases": labels.label.eq(0).sum(),
+            "num_hub_shadows": labels.label.eq(1).sum()
+        }
+
+        # The predicted labels are listed in order of confidence
+        if turbine_metadata["num_bases"] == 1 and turbine_metadata["num_hub_shadows"] == 1:
             base_x, base_y = labels.query("label == 0").iloc[0, 1:3].values
             hub_x, hub_y = labels.query("label == 1").iloc[0, 1:3].values
         else:
-            turbine_list.append(
-                {
-                    "site": site,
-                    "turbine_num": turbine_num,
-                    "actual_hub_height": actual_hub_height,
-                }
-            )
+            # Models have not detected a base and a hub
+            turbine_list.append(turbine_metadata)
             continue
 
         # Calculate shadow length and sun azimuth from labels (compass heading of the shadow)
@@ -79,15 +80,14 @@ def main():
             datetime.fromisoformat(site_metadata.photo_timestamp)
         )
         altitude, azimuth, _ = observer.at(time).observe(sun).apparent().altaz()
-        estimated_hub_height = math.tan(altitude.radians) * shadow_length
+        estimated_hub_height = round(math.tan(altitude.radians) * shadow_length, 1)
 
         turbine_list.append(
+            turbine_metadata |
             {
-                "site": site,
-                "turbine_num": turbine_num,
-                "estimated_hub_height": round(estimated_hub_height, 1),
                 "actual_hub_height": actual_hub_height,
-                "hub_height_diff": round(estimated_hub_height, 1) - actual_hub_height,
+                "estimated_hub_height": estimated_hub_height,
+                "hub_height_diff": estimated_hub_height - actual_hub_height,
                 "azimuth_diff": abs(int(shadow_azimuth) - int(azimuth.degrees)),
                 "shadow_azimuth": int(shadow_azimuth),
                 "azimuth": int(azimuth.degrees),
@@ -96,17 +96,40 @@ def main():
             }
         )
 
-    turbines = pd.DataFrame(turbine_list)
-    good_turbines = turbines[turbines.azimuth_diff.le(5) & ~turbines.actual_hub_height.isna()]
-    good_sites = good_turbines.groupby("site").agg({
-        "turbine_num": "count",
-        "actual_hub_height": "mean",
-        "estimated_hub_height": "mean",
-        "hub_height_diff": "mean",
-    }).sort_values("hub_height_diff")
-
-    print("Done")
+    turbines = pd.DataFrame(turbine_list).assign(
+        missing_labels=lambda x: x.num_bases.eq(0) | x.num_hub_shadows.eq(0),
+        multiple_labels=lambda x: (x.num_bases + x.num_hub_shadows).gt(2) & ~x.missing_labels,
+        azimuth_mismatch=lambda x: x.azimuth_diff.gt(10) & ~x.multiple_labels,
+        good_estimate=lambda x: (
+            ~x[["missing_labels", "multiple_labels", "azimuth_mismatch"]].any(axis=1)
+        )
+    )
+    site_results = (
+        turbines[turbines.good_estimate]
+        .groupby("site")
+        .agg({
+            "actual_hub_height": "mean",
+            "estimated_hub_height": "mean",
+            "hub_height_diff": "mean",
+            "altitude": "count"
+        })
+        .rename(columns={"altitude": "valid_estimates"})
+        .join(
+            turbines[~turbines.good_estimate]
+            .groupby("site")
+            .agg({
+                "missing_labels": "sum",
+                "multiple_labels": "sum",
+                "azimuth_mismatch": "sum"
+            }),
+            how="outer"
+        )
+        .assign(num_turbines=lambda x: x.iloc[:, -4:].sum(axis=1))
+    )
+    turbines.to_csv(f"data/{run_name}_turbine_predictions.csv")
+    site_results.to_csv(f"data/{run_name}_site_predictions.csv")
 
 
 if __name__ == "__main__":
-    main()
+    main("train")
+    # main("test")
